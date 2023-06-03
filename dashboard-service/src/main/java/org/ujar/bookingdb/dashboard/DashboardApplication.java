6e7e9014a82c68c7a01cce0c1069de185d7e22e9
Dima Denisenko <1253839+dimdnk@users.noreply.github.com>
2023-06-03 18:32:44 +0300

    chore: enable reasonable defaults for observability components

--- dashboard-service/src/main/java/org/ujar/bookingdb/dashboard/DashboardApplication.java
+++ dashboard-service/src/main/java/org/ujar/bookingdb/dashboard/DashboardApplication.java
@@ -2,10 +2,13 @@ package org.ujar.bookingdb.dashboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.metrics.buffering.BufferingApplicationStartup;
 
 @SpringBootApplication
 public class DashboardApplication {
   public static void main(String[] args) {
-    SpringApplication.run(DashboardApplication.class, args);
+    SpringApplication springApplication = new SpringApplication(DashboardApplication.class);
+    springApplication.setApplicationStartup(new BufferingApplicationStartup(2048));
+    springApplication.run(args);
   }
 }
--- dashboard-service/src/main/java/org/ujar/bookingdb/dashboard/config/ApplicationConfig.java
+++ dashboard-service/src/main/java/org/ujar/bookingdb/dashboard/config/ApplicationConfig.java
@@ -1,5 +1,7 @@
 package org.ujar.bookingdb.dashboard.config;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.context.annotation.Configuration;
 import org.ujar.bookingdb.jobs.annotation.EnableJobSupport;
 import org.ujar.bookingdb.persistence.annotation.EnableBookingPersistence;
@@ -7,5 +9,6 @@ import org.ujar.bookingdb.persistence.annotation.EnableBookingPersistence;
 @Configuration
 @EnableJobSupport
 @EnableBookingPersistence
+@OpenAPIDefinition(info = @Info(title = "Bookingdb Dashboard API", version = "23.0.0"))
 class ApplicationConfig {
 }
