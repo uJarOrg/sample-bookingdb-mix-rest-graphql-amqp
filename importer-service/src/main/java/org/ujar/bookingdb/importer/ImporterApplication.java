695ba0a1229a67666f7ce4a517f56f0d1b93e102
Dima Denisenko <1253839+dmitriidenisenko@users.noreply.github.com>
2023-06-03 18:32:44 +0300

    chore: enable reasonable defaults for observability components
    Signed-off-by: Dima Denisenko <1253839+dmitriidenisenko@users.noreply.github.com>

--- importer-service/src/main/java/org/ujar/bookingdb/importer/ImporterApplication.java
+++ importer-service/src/main/java/org/ujar/bookingdb/importer/ImporterApplication.java
@@ -2,10 +2,13 @@ package org.ujar.bookingdb.importer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.metrics.buffering.BufferingApplicationStartup;
 
 @SpringBootApplication
 public class ImporterApplication {
   public static void main(String[] args) {
-    SpringApplication.run(ImporterApplication.class, args);
+    SpringApplication springApplication = new SpringApplication(ImporterApplication.class);
+    springApplication.setApplicationStartup(new BufferingApplicationStartup(2048));
+    springApplication.run(args);
   }
 }
